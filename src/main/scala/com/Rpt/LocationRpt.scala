5db5b1a683d0a5d6df11c08ff9f114df095f0354
crazyPrint <962067292@qq.com>
2019-08-23 11:58:15 +0800

    标签

--- src/main/scala/com/Rpt/LocationRpt.scala
+++ src/main/scala/com/Rpt/LocationRpt.scala
@@ -1,5 +1,6 @@
 package com.Rpt
 
+import com.utils.RptUtils
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.SQLContext
 
@@ -12,12 +13,12 @@ object LocationRpt {
 
     System.setProperty("hadoop.home.dir", "D:\\Huohu\\下载\\hadoop-common-2.2.0-bin-master")
     // 判断路径是否正确
-    if(args.length != 1){
+    if(args.length != 2){
       println("目录参数不正确，退出程序")
       sys.exit()
     }
     // 创建一个集合保存输入和输出目录
-    val Array(inputPath) = args
+    val Array(inputPath,outputPath) = args
     val conf = new SparkConf().setAppName(this.getClass.getName).setMaster("local[*]")
       // 设置序列化方式 采用Kyro序列化方式，比默认序列化方式性能高
       .set("spark.serializer","org.apache.spark.serializer.KryoSerializer")
@@ -42,10 +43,24 @@ object LocationRpt {
       val pro = row.getAs[String]("provincename")
       val city = row.getAs[String]("cityname")
       // 创建三个对应的方法处理九个指标
-
-
-      ((pro,city),)
+      val reqlist = RptUtils.request(requestmode,processnode)
+      val clicklist = RptUtils.click(requestmode,iseffective)
+      val adlist = RptUtils.Ad(iseffective,isbilling,isbid,iswin,adorderid,WinPrice,adpayment)
+      ((pro,city),reqlist++clicklist++adlist)
     })
-
+      // 根据Key聚合Value
+      .reduceByKey(
+      (list1,list2)=>{
+        // list(（1,1），（2,2），（3,3）)
+      list1.zip(list2).map(t=>t._1+t._2)
+    })
+      // 整理元素
+      .map(t=>{
+      t._1+","+t._2.mkString(",")
+    })
+      .saveAsTextFile(outputPath)
+    // 如果存入mysql的话，你需要使用foreachPartition
+    // 需要自己写一个连接池
+    // 作业
   }
 }
