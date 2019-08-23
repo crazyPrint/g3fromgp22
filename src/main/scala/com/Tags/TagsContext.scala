813f7d34a7e33c9c9a5e7362fc2fafe693241edb
crazyPrint <962067292@qq.com>
2019-08-23 17:20:33 +0800

    关键词标签

--- src/main/scala/com/Tags/TagsContext.scala
+++ src/main/scala/com/Tags/TagsContext.scala
@@ -13,13 +13,22 @@ object TagsContext {
       println("目录不匹配，退出程序")
       sys.exit()
     }
-    val Array(inputPath,outputPath)=args
+    val Array(inputPath,outputPath,dirPath,stopPath)=args
     // 创建上下文
     val conf = new SparkConf().setAppName(this.getClass.getName).setMaster("local[*]")
     val sc = new SparkContext(conf)
     val sQLContext = new SQLContext(sc)
     // 读取数据
     val df = sQLContext.read.parquet(inputPath)
+    // 读取字段文件
+    val map = sc.textFile(dirPath).map(_.split("\t",-1))
+      .filter(_.length>=5).map(arr=>(arr(4),arr(1))).collectAsMap()
+    // 将处理好的数据广播
+    val broadcast = sc.broadcast(map)
+
+    // 获取停用词库
+    val stopword = sc.textFile(stopPath).map((_,0)).collectAsMap()
+    val bcstopword = sc.broadcast(stopword)
     // 过滤符合Id的数据
     df.filter(TagUtils.OneUserId)
       // 接下来所有的标签都在内部实现
@@ -28,6 +37,8 @@ object TagsContext {
           val userId = TagUtils.getOneUserId(row)
          // 接下来通过row数据 打上 所有标签（按照需求）
         val adList = TagsAd.makeTags(row)
+        val appList = TagAPP.makeTags(row,broadcast)
+        val keywordList = TagKeyWord.makeTags(row,bcstopword)
         })
 
   }
